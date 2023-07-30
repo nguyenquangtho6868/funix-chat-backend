03980fb9704c03a0bf803ed0f1d373caa63a13a5
Phuchu97 <103049585+Phuchu97@users.noreply.github.com>
2023-07-30 11:31:14 +0700

    update env

--- src/models/roomChat.js
+++ src/models/roomChat.js
@@ -4,7 +4,9 @@ const { Schema } = mongoose;
 
 const RoomChatSchema = new Schema({
     users: [{ type: mongoose.Schema.Types.ObjectId, ref: "users" }],
-    is_history: { type: Boolean, default: false}
+    is_history: { type: Boolean, default: false},
+    start_date: String,
+    end_date: String
 });
 
 const RoomChatModel = mongoose.model('rooms', RoomChatSchema);
